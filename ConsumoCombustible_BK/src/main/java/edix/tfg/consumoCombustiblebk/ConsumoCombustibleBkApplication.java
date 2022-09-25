58e78aab20b31deaf8f62c1e48bb3e076f56a4aa
JoseASantoss <j.santos.sanchez@gmail.com>
2022-09-25 18:39:43 +0200

    creación de base del proyecto Consumo de Combustible para el TFG de EDIX

---
+++ ConsumoCombustible_BK/src/main/java/edix/tfg/consumoCombustiblebk/ConsumoCombustibleBkApplication.java
@@ -0,0 +1,13 @@
+package edix.tfg.consumoCombustiblebk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoCombustibleBkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoCombustibleBkApplication.class, args);
+	}
+
+}
---
+++ ConsumoCombustible_BK/src/test/java/edix/tfg/consumoCombustiblebk/ConsumoCombustibleBkApplicationTests.java
@@ -0,0 +1,13 @@
+package edix.tfg.consumoCombustiblebk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoCombustibleBkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
