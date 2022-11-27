f3047117b49fc9b9cf121497b9438083f1e1034a
luis <luiscifuentes7@gmail.com>
2022-11-27 15:30:07 +0100

    Guardado para comparar versiones previas

--- ConsumoCombustible_BK/src/main/java/edix/tfg/consumoCombustiblebk/dao/IVehiculoDao.java
+++ ConsumoCombustible_BK/src/main/java/edix/tfg/consumoCombustiblebk/dao/IVehiculoDao.java
@@ -14,24 +14,24 @@ public interface IVehiculoDao extends JpaRepository<Vehiculo, Long> {
 	@Query("select vh from Vehiculo vh "
 			+ "where vh.usuario.usuarioId = ?1")
 	public List<Vehiculo> findVehiculosByUsuario(Long usuarioId);
-	
+			
 	@Query("SELECT vehiculos FROM Usuario usr "
-			+ "INNER JOIN usr.vehiculos uv"
+			+ "INNER JOIN usr.vehiculos uv "
 			+ "WHERE uv.usuarioId = ?1 "
 			+ "AND Vehiculo.vehiculoMatricula LIKE %?2%")
 	public List<Vehiculo> busquedaVehiculosUsuario(Long usuarioId, String busqueda);
 	
 	@Query("SELECT vh from Vehiculo vh "
 			+ "WHERE vh.versionCoche.versionNombre LIKE %?1% "
-			+ "OR vh.versionCoche.modelosCoche.modeloNombre LIKE %?1% "
-			+ "OR vh.versionCoche.modelosCoche.marcasCoche.marcaNombre LIKE %?1% ")
+			+ "OR vh.versionCoche.modeloCoche.modeloNombre LIKE %?1% "
+			+ "OR vh.versionCoche.modeloCoche.marcaCoche.marcaNombre LIKE %?1% ")
 	public List<Vehiculo> busquedaVehiculosDescripcion(String busqueda);
 
 	@Query("SELECT vh from Vehiculo vh "
 			+ "WHERE vh.usuario.usuarioId = ?1 "
 			+ "AND (vh.versionCoche.versionNombre LIKE %?2% "
-			+ "OR vh.versionCoche.modelosCoche.modeloNombre LIKE %?2% "
-			+ "OR vh.versionCoche.modelosCoche.marcasCoche.marcaNombre LIKE %?2% )")
+			+ "OR vh.versionCoche.modeloCoche.modeloNombre LIKE %?2% "
+			+ "OR vh.versionCoche.modeloCoche.marcaCoche.marcaNombre LIKE %?2% )")
 	public List<Vehiculo> busquedaVehiculosUsuarioDescripcion(Long usuarioId, String busqueda);
 
 }
